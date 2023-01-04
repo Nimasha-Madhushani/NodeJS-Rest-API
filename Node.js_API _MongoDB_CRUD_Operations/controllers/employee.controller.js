78e9c9681eca109215d11bc9494920ac06b9e4ec
Nimasha-Madhushani <nimashamadhushani19@gmail.com>
2023-01-04 16:41:18 +0530

    get specific employee

--- Node.js_API _MongoDB_CRUD_Operations/controllers/employee.controller.js
+++ Node.js_API _MongoDB_CRUD_Operations/controllers/employee.controller.js
@@ -8,10 +8,24 @@ router.get("/", (req, res) => {
     .catch((err) => console.log(err));
 });
 
+router.get("/:id", (req, res) => {
+  Employee.findById(req.params.id)
+    .then((data) => {
+      if (data) {
+        res.send(data);
+      } else {
+        res.status(404).json({
+          error: "no record with the given id",
+        });
+      }
+    })
+    .catch((err) => console.log(err));
+});
+
 router.post("/", (req, res) => {
   //req.body
   Employee.create(req.body)
-    .then((data) => res.send(data))
+    .then((data) => res.status(201).json(data))
     .catch((err) => console.log(err));
 });
 module.exports = router;
